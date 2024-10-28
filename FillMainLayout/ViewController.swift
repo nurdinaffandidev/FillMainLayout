3f3f60259179beddc291e2e910d8e2a5b6cb385b
nurdinaffandidev <nurdinaffandidev@gmail.com>
2024-10-28 23:40:58 +0800

    feat[FillMainContentViewController]: implementation

--- FillMainLayout/ViewController.swift
+++ FillMainLayout/ViewController.swift
@@ -5,15 +5,4 @@
 //  Created by nurdin affandi on 28/10/24.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+import Foundation
